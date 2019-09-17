76d8938d8ffd83d7cb02729b8752dd55da4764a5
trinhtuantu98 <trinhtuantu98@gmail.com>
2019-09-17 22:20:29 +0700

    them so lan ra san

--- models/lineup.py
+++ models/lineup.py
@@ -15,33 +15,34 @@ doi = [{'name':'Napoli','number':299},{'name':'Liverpool','number':232},
 clear_database()
 browser = webdriver.Chrome()
 while True:
-    for o in doi:
-        lineup = []
-        result = []
-        browser.get('https://baomoi.com/soccer/d/team/{}'.format(o['number']))
-        time.sleep(5)
-        a = browser.find_elements_by_xpath('//div[@class="name large-link player"]/a')
-        b = browser.find_elements_by_xpath('//div[@class="position large-link"]/span')
-        c = browser.find_elements_by_xpath('//div[@class="number statistic goals available"]')
-        d = browser.find_elements_by_xpath('//div[@class="number statistic assists"]')
-        e = browser.find_elements_by_xpath('//div[@class="number statistic yellow-cards available"]')
-        f = browser.find_elements_by_xpath('//div[@class="number statistic red-cards available"]')
-        g = browser.find_elements_by_xpath('//div[@class="photo"]/a/img')
-        h = browser.find_elements_by_xpath('//div[@class="shirtnumber"]')
-        stt = 0
-        for v in range(len(a)):
-            lineup.append({})
-            lineup[stt]['ten'] = a[v].text
-            lineup[stt]['vitri'] = b[v].text
-            lineup[stt]['banthang'] = c[v+1].text
-            lineup[stt]['assist']= d[v].text
-            lineup[stt]['thevang']= e[v+1].text
-            lineup[stt]['thedo']= f[v+1].text
-            lineup[stt]['ava']=g[v].get_attribute('src')
-            lineup[stt]['soao']=h[v+1].text
-            stt += 1
-        for v in range(6,11):
-            result.append(browser.find_element_by_xpath('//div[@class="row row--data row--grey"][{}]'.format(v)).text)
-        insert_lineup(o['name'],lineup,result)
-        print(o['name'],'xong')
-        time.sleep(86400)
+	for o in doi:
+	    lineup = []
+	    result = []
+	    browser.get('https://baomoi.com/soccer/d/team/{}'.format(o['number']))
+	    a = browser.find_elements_by_xpath('//div[@class="name large-link player"]/a')
+	    b = browser.find_elements_by_xpath('//div[@class="position large-link"]/span')
+	    c = browser.find_elements_by_xpath('//div[@class="number statistic goals available"]')
+	    d = browser.find_elements_by_xpath('//div[@class="number statistic assists"]')
+	    e = browser.find_elements_by_xpath('//div[@class="number statistic yellow-cards available"]')
+	    f = browser.find_elements_by_xpath('//div[@class="number statistic red-cards available"]')
+	    g = browser.find_elements_by_xpath('//div[@class="photo"]/a/img')
+	    h = browser.find_elements_by_xpath('//div[@class="shirtnumber"]')
+	    i = browser.find_elements_by_xpath('//div[@class="number statistic appearances available"]')
+	    stt = 0
+	    for v in range(len(a)):
+	        lineup.append({})
+	        lineup[stt]['ten'] = a[v].text
+	        lineup[stt]['vitri'] = b[v].text
+	        lineup[stt]['banthang'] = c[v+1].text
+	        lineup[stt]['assist']= d[v].text
+	        lineup[stt]['thevang']= e[v+1].text
+	        lineup[stt]['thedo']= f[v+1].text
+	        lineup[stt]['ava']=g[v].get_attribute('src')
+	        lineup[stt]['soao']=h[v+1].text
+	        lineup[stt]['solanrasan'] =i[v+1].text
+	        stt += 1
+	    for v in range(6,11):
+	        result.append(browser.find_element_by_xpath('//div[@class="row row--data row--grey"][{}]'.format(v)).text)
+	    insert_lineup(o['name'],lineup,result)
+	    print(o['name'],'xong')
+	    time.sleep(86400)
